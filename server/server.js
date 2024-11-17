e591762976be52e44831271691865e9eb8dd086c
golda <z0504199417@gmail.com>
2024-11-17 16:08:17 +0200

    add cors

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDB = require("./config/dbConn");
+const corsOptions = require("./config/corsOption");
 
 const app = express();
 
