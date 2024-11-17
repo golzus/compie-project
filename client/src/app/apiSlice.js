d62bb7f67c000f57ec1d1bff8ea45f74d095ca0d
golda <z0504199417@gmail.com>
2024-11-17 14:56:06 +0200

    uploading the website

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseQuery = fetchBaseQuery({
- baseUrl: 'http://localhost:1000',
-  
+//  baseUrl: 'http://localhost:1000',
+ baseUrl: 'https://java-springboot-owxn.onrender.com'
 })
 const apiSlice = createApi({
     reducerPath: 'api',
